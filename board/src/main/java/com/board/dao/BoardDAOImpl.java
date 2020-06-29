899007969f813ec1584b4327cea9addbbd8a2ab6
cjfrn <cjfrn0443@gmail,com>
2020-06-29 17:53:13 +0900

    Add Paging

--- board/src/main/java/com/board/dao/BoardDAOImpl.java
+++ board/src/main/java/com/board/dao/BoardDAOImpl.java
@@ -1,5 +1,6 @@
 package com.board.dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -44,6 +45,23 @@ public class BoardDAOImpl implements BoardDAO {
 	@Override
 	public void delete(int bno) throws Exception {
 		sql.delete(namespace + ".delete", bno);
+	}
+
+	@Override
+	public int count() throws Exception {
+		return sql.selectOne(namespace + ".count");
+	}
+
+	@Override
+	public List<BoardVO> listPage(int displayPost, int postNum) throws Exception {
+		
+		HashMap<String, Integer> data = new HashMap<String, Integer>();
+		
+		data.put("displayPost", displayPost);
+		data.put("postNum", postNum);
+		
+		return sql.selectList(namespace + ".listPage", data);
+		
 	}
 
 }
